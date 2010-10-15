dfc1b16539d69a8aee7fc55ecc26756f1bcd5570
J.P. Cummins <jpcummins@gmail.com>
2010-10-14 21:48:09 -0700

    Added heap sort and an additional test case with 5000 random numbers.

--- includes.h
+++ includes.h
@@ -13,3 +13,5 @@ extern void test_int_sort(void (*sort)(void**, size_t, int (*cmp)(void*, void*))
 extern void insert_sort(void* array[], size_t size, int (*cmp)(void*, void*));
 
 extern void bubble_sort(void* array[], size_t size, int (*cmp)(void*, void*));
+
+extern void heap_sort(void* array[], size_t size, int (*cmp)(void*, void*));
